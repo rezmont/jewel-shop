714ca9e45ae83bb16148750ba7fcc373145507fb
rezmont <reza.motamedi@gmail.com>
2020-04-20 21:46:30 -0700

    Updates

---
+++ precache-manifest.c23113238a2c857f80b21268c8c07072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff42eb455fac12aae47a3dba4be0b79",
+    "url": "/jewel-shop/index.html"
+  },
+  {
+    "revision": "1c88df9fb72157f9bfb2",
+    "url": "/jewel-shop/static/css/main.d51ce19e.chunk.css"
+  },
+  {
+    "revision": "f73fc1751453af2af99b",
+    "url": "/jewel-shop/static/js/2.877774dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jewel-shop/static/js/2.877774dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c88df9fb72157f9bfb2",
+    "url": "/jewel-shop/static/js/main.40723ede.chunk.js"
+  },
+  {
+    "revision": "4d3edfea1ee0416b7e45",
+    "url": "/jewel-shop/static/js/runtime-main.cd67221f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jewel-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
